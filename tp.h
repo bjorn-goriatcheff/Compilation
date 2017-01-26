79fd1e89292bc79df7546a312fc18ea98fff4e82
theolaf <theo.lafforgue@gmail.com>
2017-01-26 15:42:51 +0100

    Update tp.h

--- tp.h
+++ tp.h
@@ -159,7 +159,6 @@ TreeP makeBlock(VarDeclP decl, TreeP instr);
 MethodP fillMeth(MethodP meth, TreeP bloc);
 CorpP makeCorps(VarDeclP decl, MethodP meth);
 ClassP makeClass(char* name, VarDeclP var, char* super);
-ClassP fillClass(ClassP class, char* super, TreeP bloc);
 TeteP makeTete(char* nom, VarDeclP var);
 
 void makeProg(ClassP listC, MethodP listM);
