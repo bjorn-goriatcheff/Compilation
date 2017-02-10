fe4be65e537993fafb17a3939beeee56e016fd11
CYLAHI <cyrille.laure@gmail.com>
2017-02-10 11:02:48 +0100

    Add files via upload

--- code.c
+++ code.c
@@ -4,3 +4,18 @@
 #include "tp_y.h"
 
 extern char* strdup(const char *);
+
+int etifin=0;
+
+
+//Création des etiquettes pour la génération de code
+
+char* newLabel(){
+	
+
+	char* eti=NULL;
+	printf("Etiquette : %d",etifin);
+	etifin=etifin+1;
+	return eti;
+	
+}
